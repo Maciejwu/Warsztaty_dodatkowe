2f609ca03b1d4d3dc5d51cd5d73904b7aee5efdc
Maciej Wu <maciej.wolkowycki@gmail.com>
2017-02-06 15:48:24 +0100

    Add files via upload
    test

---
+++ class/User.php
@@ -0,0 +1,85 @@
+<?php
+  /**
+  * Class represent one user;
+  */
+  class User{
+
+    private $id;
+    private $address;
+    private $name;
+    private $surname;
+    private $credits;
+    private $hashedPassword;
+
+    public function __construct(){
+        $this->$id = -1;
+        $this->address = '';
+        $this->name = '';
+        $this->surname = '';
+        $this->credits = null;
+        $this->hashedPassword = '';
+    }
+    public function getId()
+    {
+      return $this->id;
+    }
+    public function getAddress()
+    {
+      return $this->address;
+    }
+    public function setAddress($address)
+    {
+      if(is_string($address)){
+        $this->address = $address;
+        return true;
+      }
+    }
+    public function getName()
+    {
+      return $this->name;
+    }
+    public function setName($name)
+    {
+      if(is_string($name)){
+        $this->name = $name;
+        return true;
+      }
+    }
+    public function getSurname()
+    {
+      return $this->surname;
+    }
+    public function setSurname($surname)
+    {
+      if(is_string($surname)){
+        $this->surname = $surname;
+        return true;
+      }
+    }
+    public function getCredits()
+    {
+      return $this->credits;
+    }
+    public function setCredits($credits)
+    {
+      if(is_string($credits)){
+        $this->credits = $credits;
+        return true;
+      }
+    }
+    public function getHashedPassword()
+    {
+      return $this->hashedPassword;
+    }
+    public function setHashedPassword($hashedPassword)
+    {
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        $this->hashedPassword = $passwordhashed;
+        return true;
+    }
+
+}
+
+
+
+?>
